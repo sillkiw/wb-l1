28e5d142675cda2435033c34130d957bf6c18c86
sillkiw <anatoliy.mepost978@gmail.com>
2025-11-02 19:52:58 +0300

    t16

---
+++ task16/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{2, 3, 4, 5, 1, 2, 3, 4}
+	fmt.Println(QuickSort(a))
+}
+
+func QuickSort(a []int) []int {
+	return quickSortR(a, 0, len(a)-1)
+}
+
+func quickSortR(a []int, low, high int) []int {
+	if low < high {
+		var pivot int
+		a, pivot = partition(a, low, high)
+		a = quickSortR(a, low, pivot-1)
+		a = quickSortR(a, pivot+1, high)
+	}
+	return a
+}
+
+func partition(a []int, low, high int) ([]int, int) {
+	pivot := a[high]
+	i := low
+	for j := low; j < high; j++ {
+		if a[j] < pivot {
+			a[i], a[j] = a[j], a[i]
+			i++
+		}
+	}
+	a[i], a[high] = a[high], a[i]
+	return a, i
+}
